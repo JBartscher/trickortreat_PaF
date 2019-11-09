d039ab36ef493462a82bfadd3df6cb235b1cc025
JB <jasper.bartscher@gmail.com>
2019-11-09 21:47:07 +0100

    comments for House class

--- src/main/java/gameobjects/House.java
+++ src/main/java/gameobjects/House.java
@@ -8,12 +8,26 @@ import java.util.Arrays;
 
 public class House {
 
+    /**
+     Offset of Object to other Objects @see Placeble
+     */
     final int OFFSET = 1;
 
     private Tile[][] tileset;
 
     private Placeble placeble;
 
+    private Tile doorTile;
+
+    /**
+     * House constructor.
+     *
+     * fills the whole tileset with House-Tiles and creates a Door-Tile at a suitable place.
+     * @param x x coordinate on game map
+     * @param y y coordinate on game map
+     * @param tileWidth width
+     * @param tileHeight height
+     */
     public House(int x, int y, int tileWidth, int tileHeight) {
         this.tileset = new Tile[tileWidth][tileHeight];
         for (Tile[] row : this.tileset)
@@ -22,6 +36,11 @@ public class House {
         setHouseDoorPosition();
     }
 
+    /**
+     * House constructor in which all coordiantes are set by its placeble Object.
+     *
+     * @param placeble Placeble
+     */
     public House(Placeble placeble) {
         this.tileset = new Tile[placeble.getWidth()][placeble.getHeight()];
         for (Tile[] row : this.tileset)
@@ -30,18 +49,39 @@ public class House {
         setHouseDoorPosition();
     }
 
+    /**
+     * gets the placeble of the House instance.
+     *
+     * @return Placeble
+     */
     public Placeble getPlaceble() {
         return placeble;
     }
 
+    /**
+     * Sets the Tile at the last Row in the relative center of the House to a House Tile Object and sets a refrence
+     * to this Tile.
+     */
     private void setHouseDoorPosition() {
         int center_cell = tileset.length / 2;
         int last_row = tileset[0].length - 1;
 
         tileset[last_row][center_cell] = TileCollection.DOOR_TILE;
+        this.doorTile = tileset[last_row][center_cell];
     }
 
-    public Tile getHouseTile(int r, int c) {
-        return tileset[r][c];
+    /**
+     * gets an Tile of the House by its row and column index
+     * @param r row index
+     * @param c column index
+     * @return the tile that is indexed by r and c
+     */
+    public Tile getTileByTileIndex(int r, int c) {
+        try{
+        return tileset[r][c];}
+        catch (ArrayIndexOutOfBoundsException e){
+            e.printStackTrace();
+            return null;
+        }
     }
 }
