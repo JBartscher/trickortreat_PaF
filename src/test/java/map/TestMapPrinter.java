aab3d03ebf66ada0a81e483882881013cc04f42c
JB <jasper.bartscher@gmail.com>
2019-11-09 22:41:30 +0100

    static MapPrinterMethod to print a map to the console.

--- src/test/java/map/TestMapPrinter.java
+++ src/test/java/map/TestMapPrinter.java
@@ -3,6 +3,11 @@ package map;
 public class TestMapPrinter {
 
     static void printMap(Map map){
-
+        for(int i = 0; i < map.map[0].length; i++){
+            for(int j = 0; j < map.map.length; j++){
+                System.out.print(map.map[i][j]);
+            }
+            System.out.print("\n");
+        }
     }
 }
