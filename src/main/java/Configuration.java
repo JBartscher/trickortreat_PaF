4af91058fd46bb9587ae245badcd42adb2760e55
Marcel Pawlowski <marcel.pawlowski@hotmail.de>
2020-01-22 21:01:24 +0100

    comments added

--- src/main/java/Configuration.java
+++ src/main/java/Configuration.java
@@ -6,24 +6,32 @@ import org.json.simple.parser.JSONParser;
 import java.io.FileReader;
 import java.io.PrintWriter;
 
+/**
+ * class to read and write parameters in json file
+ * @param <P>
+ */
 public class Configuration<P> {
 
+    private final static String configPath = "src/main/java/configuration/config.json";
+
+    /**
+     * get parameters
+     * @param key
+     * @return
+     */
     public P getParam(String key) {
 
         P param = null;
 
         try {
             // parsing file
-            Object obj = new JSONParser().parse(new FileReader("src/main/java/configuration/config.json"));
+            Object obj = new JSONParser().parse(new FileReader(configPath));
 
             // typecasting obj to JSONObject
             JSONObject jo = (JSONObject) obj;
 
             // get param
             param = (P) jo.get(key);
-
-            // print param
-            // System.out.println(key + ": " + param);
 
         } catch (Exception e) {
             System.out.println(e);
@@ -32,11 +40,16 @@ public class Configuration<P> {
         return param;
     }
 
+    /**
+     * set parameters
+     * @param key
+     * @param value
+     */
     public void setParam(String key, P value) {
 
         try {
             // parsing file
-            Object obj = new JSONParser().parse(new FileReader("src/main/java/configuration/config.json"));
+            Object obj = new JSONParser().parse(new FileReader(configPath));
 
             // typecasting obj to JSONObject
             JSONObject jo = (JSONObject) obj;
@@ -45,7 +58,7 @@ public class Configuration<P> {
             jo.put(key, value); 
 
             // writing JSON to file
-            PrintWriter pw = new PrintWriter("src/main/java/configuration/config.json"); 
+            PrintWriter pw = new PrintWriter(configPath); 
             pw.write(jo.toJSONString()); 
             pw.flush(); 
             pw.close();
